95a50915a52190e61ac67b0d911d09ea7f7cf762
Shashank Shekhar <sh16011993@gmail.com>
2020-06-13 00:51:39 -0400

    Modified Prototype 2 and Added Game Design Documents

--- Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DestroyOutofBounds : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class DestroyOutofBounds : MonoBehaviour
         }
         else if(transform.position.z < lowerBound){
             Destroy(gameObject);
-            Debug.Log("Game Over!");
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
